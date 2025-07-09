a802cb742d7828576196c235dec3c6eab3d6dc09
Andy Tyler <andrewjtyler1@gmail.com>
2025-07-09 16:47:26 +0100

    structure uipdate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,25 @@
 <script lang="ts">
-  import { navigating, page } from "$app/stores"
-  import { Separator } from "$components/ui/separator"
-  import * as Icon from "@tabler/icons-svelte"
   import { ModeWatcher } from "mode-watcher"
   import type { Snippet } from "svelte"
 
-
   import "../app.css"
+  import Footer from "./Footer.svelte"
+  import Header from "./Header.svelte"
 
   interface Props {
-    children?: Snippet
+    children: Snippet
     data?: any
   }
 
   let { children, data }: Props = $props()
 </script>
 
-  <ModeWatcher />
+<ModeWatcher />
 
-  <div class="flex flex-col min-h-screen">
-    <Header />
-    <main class="flex-1">
-      {@render children()}
-    </main>
-    <Footer />
-  </div>
+<div class="flex min-h-screen flex-col">
+  <Header />
+  <main class="flex-1">
+    {@render children()}
+  </main>
+  <Footer />
+</div>
