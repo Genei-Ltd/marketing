0c352e5558eff12b217489497014683516dcb7cc
Andy Tyler <andrewjtyler1@gmail.com>
2025-07-09 16:39:45 +0100

    setup

--- src/routes/(independents)/demo/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@sveltejs/kit"
-
-export async function load({ locals }) {
-  const { userId } = locals.auth()
-
-  if (userId) {
-    return redirect(307, "/billing")
-  }
-}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,1 @@
-import { redirect } from "@sveltejs/kit"
-
-export const load = async ({ locals }) => {
-  const { userId } = locals.auth()
-  if (userId) {
-    redirect(302, "/billing")
-  } else {
-    redirect(302, "/sign-in")
-  }
-}
+export const load = async ({ locals }) => {}
