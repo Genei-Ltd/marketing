273a87d0dfaa04df4317a5fa8a6552a5350da235
Andy Tyler <andrewjtyler1@gmail.com>
2025-08-12 16:33:28 +0100

    SEO: SEO and Social Media Images

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,9 @@ export const DOCS_URL: string = "https://docs.coloop.ai"
 
 export const WebsiteDescription: string = "Ai Research for Insights Teams."
 export const LOGO: string = "/logo.png"
+
+// SEO and Social Media Images
+export const DEFAULT_OG_IMAGE: string = "/images/og-image.png"
+export const BLOG_OG_IMAGE: string = "/images/blog-og.png"
+export const BLOG_CATEGORY_OG_IMAGE: string = "/images/blog-category-og.png"
+export const BLOG_POST_OG_IMAGE: string = "/images/blog-post-og.png"
