9dcb56e671b2086004ba2b91fd6fd89e559100f9
Andy Tyler <andrewjtyler1@gmail.com>
2025-08-19 15:39:45 +0100

    truncate git commit SHA to first 7 characters for display

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,7 +4,7 @@ import { RAILWAY_GIT_COMMIT_SHA, RAILWAY_GIT_AUTHOR } from "$env/static/private"
 export const load: LayoutServerLoad = () => {
 	return {
 		gitData: {
-			RAILWAY_GIT_COMMIT_SHA,
+			RAILWAY_GIT_COMMIT_SHA: RAILWAY_GIT_COMMIT_SHA.slice(0, 7),
 			RAILWAY_GIT_AUTHOR,
 		},
 	}
