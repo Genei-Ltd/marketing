084c93ff5d2b5c10645d6a2508541ce17b5fc1cb
Andy Tyler <andrewjtyler1@gmail.com>
2025-08-20 22:49:42 +0100

    upsert where slug is eq

--- src/lib/server/notion-supabase.ts
+++ src/lib/server/notion-supabase.ts
@@ -25,7 +25,7 @@ function transformArticleToSupabaseBlogPost(article: Article): SupabaseBlogPost 
 export async function upsertBlogPost(blogPost: Article) {
 	const blog_as_supabase = transformArticleToSupabaseBlogPost(blogPost)
 
-	const { data, error } = await supabase.from("blogs").upsert(blog_as_supabase).select()
+	const { data, error } = await supabase.from("blogs").upsert(blog_as_supabase).eq("slug", blogPost.slug).select()
 
 	if (error) {
 		console.error(error)
