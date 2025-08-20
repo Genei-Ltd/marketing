80b4eb1510640924e47efc770af969f97282dcd4
Andy Tyler <andrewjtyler1@gmail.com>
2025-08-20 15:34:26 +0100

    update redirects

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const { url } = event;
-	if (url.pathname.startsWith("/news/")) {
+	if (url.pathname.startsWith("/news/") || url.pathname.startsWith("/coloop-blog/")) {
 		const newPath = url.pathname.replace("/news/", "/blog/");
 		throw redirect(308, newPath);
 	}
