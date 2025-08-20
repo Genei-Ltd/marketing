1135c5142dab4b5c4fd29a3c3f65f629702ffbe2
Andy Tyler <andrewjtyler1@gmail.com>
2025-08-20 15:27:40 +0100

    redirect news to blog

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,12 @@
+import { redirect } from "@sveltejs/kit";
+import type { Handle } from "@sveltejs/kit";
 
+export const handle: Handle = async ({ event, resolve }) => {
+	const { url } = event;
+	if (url.pathname.startsWith("/news/")) {
+		const newPath = url.pathname.replace("/news/", "/blog/");
+		throw redirect(308, newPath);
+	}
+
+	return resolve(event);
+};
