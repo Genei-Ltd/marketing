5b146447ce3f5fe476107c9f05a0b950812a8384
Andy Tyler <andrewjtyler1@gmail.com>
2025-08-21 14:47:37 +0100

    update redirects

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,5 +8,14 @@ export const handle: Handle = async ({ event, resolve }) => {
 		throw redirect(308, newPath);
 	}
 
+	if (url.pathname.startsWith("/responsible-disclosure-policy")) {
+		const newPath = url.pathname.replace("/responsible-disclosure-policy", "/legal/responsible-disclosure-policy");
+		throw redirect(308, newPath);
+	}
+
+	if (url.pathname.startsWith("/company") || url.pathname.startsWith("/about")) {
+		throw redirect(308, "/blog");
+	}
+
 	return resolve(event);
 };
